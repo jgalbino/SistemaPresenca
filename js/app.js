2b41a01a93e2d82201fa4111f1cd3df8224c73eb
João Gabriel Albino Maruska <64925552+jgalbino@users.noreply.github.com>
2024-03-30 19:12:29 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,8 +7,13 @@ var alunosRef = db.collection("alunos");
 // Obter todos os documentos da coleção
 alunosRef.get().then(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
-        // Exibir dados na página HTML
-        document.getElementById("data").innerHTML += "<pre>" + JSON.stringify(doc.data(), null, 2) + "</pre>";
+        if (doc.exists) {
+            // Exibir dados na página HTML
+            var data = doc.data();
+            document.getElementById("data").innerHTML += "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+        } else {
+            console.log("Documento não encontrado:", doc.id);
+        }
     });
 }).catch(function(error) {
     console.log("Erro ao obter documentos:", error);
