86d2a9a10c5f6961c964e5b7e65d27828f69a42b
João Gabriel Albino Maruska <64925552+jgalbino@users.noreply.github.com>
2024-03-30 17:25:55 -0300

    Update viewAttendance.js

--- js/viewAttendance.js
+++ js/viewAttendance.js
@@ -1,3 +1,7 @@
+const firestore = firebase.firestore();
+const settings = {/* your settings... */ timestampsInSnapshots: true};
+  firestore.settings(settings);
+
 findTransactions();
 
 function findTransactions() {
