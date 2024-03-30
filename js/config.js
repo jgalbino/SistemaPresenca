dd36025809d738198def25433514bdb5a370c6a9
João Gabriel Albino Maruska <64925552+jgalbino@users.noreply.github.com>
2024-03-30 13:20:34 -0300

    1° Upload

---
+++ js/config.js
@@ -0,0 +1,9 @@
+var config = {
+	apiKey: "AIzaSyDTVuDV0-cK9Nk6OvRV3IO8f563nPXTjuY",
+	authDomain: "sistemaoctogono.firebaseapp.com",
+	projectId: "sistemaoctogono",
+	storageBucket: "sistemaoctogono.appspot.com",
+	messagingSenderId: "415747300285",
+	appId: "1:415747300285:web:2ae6ae2d51eefc7e1950d4"
+};
+firebase.initializeApp(config);
